3236301aa544643cd44bcc9cecc3981c1b0a187f
Muhammad Ihsan <muhihsan0@outlook.com>
2024-12-09 10:23:16 +0700

    succesfully create aggregations table on db

--- python_scripts/salary_survey_consumer.py
+++ python_scripts/salary_survey_consumer.py
@@ -1,9 +1,22 @@
-from helper.postgres_helper import quick_command
+from helper.postgres_helper import quick_command, create_connection
+
+def create_tables(conn, curr):
+    curr.execute("""
+    CREATE TABLE IF NOT EXISTS earnings_per_job (
+        JOB_GROUP VARCHAR(50),
+        AVERAGE_SALARY FLOAT
+    );""")
+
+    curr.execute("""
+    CREATE TABLE IF NOT EXISTS industry_earnings (
+        INDUSTRY_GROUP VARCHAR(50),
+        AVERAGE_SALARY FLOAT
+    );""")
+
+    conn.commit()
 
 if __name__ == "__main__":
-    users_summary_query = """
-    CREATE TABLE IF NOT EXISTS users_summary (
-        POKEMON VARCHAR(50)
-"""
+    conn = create_connection("localhost", "5432", "salary_survey_db", "salary_survey", "secret")
+    curr = conn.cursor()
 
-    quick_command(users_summary_query, "localhost", "5432", "salary_survey_db", "salary_survey", "secret")
+    create_tables(conn, curr)
